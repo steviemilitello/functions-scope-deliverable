9d3e0f60aede58fdcb91218c5244e566b1376ce8
STEVIEMILITELLO <steviemilitello@gmail.com>
2022-02-10 22:34:51 -0500

    commit 1 - verbal questions

---
+++ app.js
@@ -0,0 +1,8 @@
+// 1. What is the difference between a parameter and an arguement? 
+// a parameter is the input the function is taken, while the argument the specific value you want the function to output
+
+// 2. Within a function, what is the difference between return and console.log? 
+// return will return what the function is doing and can be used in data, while console.log will log into the console 
+
+// 3. What are the implications of the ability of a function to return a value? 
+// the value can be anything that matches the arguement and it will output the data 
